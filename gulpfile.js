7bcd3c350b46708b6dc19614785d0d5ecaa87acb
Kristopher Glassmire <kglassmire@gmail.com>
2020-01-04 22:39:06 -0500

    updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var nbgv = require('nerdbank-gitversioning')
 
 gulp.task('default', function() {
+    console.log("just printing version for sanity check...");
+    
     nbgv.getVersion()
     .then(r => console.log(r))
     .catch(e => console.error(e));
-    return nbgv.getVersion();
-    //return nbgv.setPackageVersion();
+    
+    return nbgv.setPackageVersion();
 });
